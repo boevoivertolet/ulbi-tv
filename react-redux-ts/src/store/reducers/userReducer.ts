02a8dafbb195a9e7d97bf959e66c17f35981aa80
boevoivertolet <bjha78rus@yandex.ru>
2023-04-06 14:31:03 +0300

    create Actions for userReducer

---
+++ react-redux-ts/src/store/reducers/userReducer.ts
@@ -0,0 +1,24 @@
+import {UserActionType, UserState} from "../../types/todo";
+
+const initialState: UserState = {
+    users: [],
+    loading: false,
+    error: null
+}
+export const userReducer = (state: UserState = initialState, action: UserActionType): UserState => {
+    switch (action.type) {
+        case 'FETCH_USERS':
+            return {...state, loading: true}
+        case 'FETCH_USERS_SUCCESS':
+            return {...state, loading: false, users: action.payload}
+        case 'FETCH_USERS_ERROR':
+            return {...state, loading: false, error: action.payload}
+        default:
+            return state
+
+    }
+}
+// Action Creators
+export const fetchUsers = () => ({type: 'FETCH_USERS' as const})
+export const fetchUsersSuccess = (users: Array<any>) => ({type: 'FETCH_USERS_SUCCESS' as const, payload: users})
+export const fetchUsersError = (error: string) => ({type: 'FETCH_USERS_ERROR' as const, payload: error})
